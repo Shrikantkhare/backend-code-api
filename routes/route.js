fc03663a07191c55376941647fef1bc1df61c1dc
Shrikantkhare <kantshri561@gmail.com.com>
2023-05-16 09:58:41 +0530

    tee

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController")
 const productController =require("../controllers/productController");
 
-router.post("/", userController.welcome)
+router.get("/", userController.welcome);
 router.post("/register", userController.registerUser)
 router.post("/add-products", productController.addProduct)
 router.get("/products", productController.getAllproducts)
